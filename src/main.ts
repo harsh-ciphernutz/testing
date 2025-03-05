362550663a7cf92ada4246bf2b2e5bc190fa26a8
harsh-ciphernutz <harsh@ciphernutz.com>
2025-03-05 12:39:03 +0530

    testing

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // Allow frontend origin
+    origin: process.env.ORIGIN, // Allow frontend origin
     credentials: true, // Allow cookies if needed
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
